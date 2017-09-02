f6a3a721ab57d1245db723cebe3324529061f4ab
khai96_ <hvksmr1996@gmail.com>
2017-09-03 01:23:23 +0700

    Complete ClockForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import AppBar from 'material-ui/AppBar'
 import DefaultMuiTheme from './DefaultMuiTheme.jsx'
 import RandomNumberForm from './RandomNumberForm.jsx'
+import ClockForm from './ClockForm.jsx'
 
 export default function App ({init}) {
   return <DefaultMuiTheme><div>
     <AppBar title='Hello, World!!' showMenuIconButton={false} />
     <RandomNumberForm init={init} />
+    <ClockForm />
   </div></DefaultMuiTheme>
 }
