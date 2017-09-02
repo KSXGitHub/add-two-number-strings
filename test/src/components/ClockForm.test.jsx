125078250749d0112db01821b9369434333332be
khai96_ <hvksmr1996@gmail.com>
2017-09-03 02:22:45 +0700

    No snapshots, no troubles

--- test/src/components/ClockForm.test.jsx
+++
@@ -1,9 +0,0 @@
-import './__lib__/material-ui-mocks.jsx'
-import React from 'react'
-import renderer from 'react-test-renderer'
-import ClockForm from '../../../src/components/ClockForm.jsx'
-
-test('Snapshot: components/ClockForm.jsx', () => {
-  const tree = renderer.create(<ClockForm />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
