8d778ab5e2e1ccce047d963b837e1419b7adddd0
khai96_ <hvksmr1996@gmail.com>
2017-09-04 00:52:34 +0700

    Create NamedRadixes and NamedValues

---
+++ src/components/NamedRadixes.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import NamedValues from './NamedValues.jsx'
+
+const DEFAULT_NAMES = {
+  2: 'Binary',
+  8: 'Octal',
+  10: 'Decimal',
+  16: 'Hexadecimal'
+}
+
+export default class NamedRadixes extends React.Component {
+  constructor (props) {
+    super(props)
+
+    const {
+      radix = 10,
+      names = DEFAULT_NAMES,
+      onChange = () => {},
+      ...rest
+    } = props
+
+    this.state = {radix, names, onChange, ...rest}
+  }
+
+  render () {
+    const {radix, names, ...rest} = this.state
+
+    const namedValues = new Map()
+    for (const radix in names) {
+      namedValues.set(radix, names[radix])
+    }
+
+    return <NamedValues
+      value={radix}
+      namedValues={namedValues}
+      onChange={radix => {
+        this.setState({radix})
+        this.state.onChange(radix)
+      }}
+      {...rest}
+    />
+  }
+}
---
+++ src/components/NamedValues.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import TextField from 'material-ui/TextField'
+import SelectField from 'material-ui/SelectField'
+import MenuItem from 'material-ui/MenuItem'
+
+export default class NamedValues extends React.Component {
+  constructor (props) {
+    super(props)
+
+    const {
+      value,
+      namedValues = new Map(),
+      defaultName = '',
+      onChange = () => {},
+      textFieldProps = {},
+      selectFieldProps = {},
+      createMenuItemProps = () => ({}),
+      container = props => (<div {...props} />)
+    } = props
+
+    this.state = {
+      onChange,
+      textFieldProps,
+      selectFieldProps,
+      createMenuItemProps,
+      container,
+      ...this.createState(value, namedValues, defaultName)
+    }
+  }
+
+  render () {
+    const Container = this.state.container
+    const onChange = (_, value) => {
+      let prevent = false
+
+      this.state.onChange(value, {
+        preventDefault: () => { prevent = true }
+      })
+
+      prevent || this.setState(
+        this.createState(value, this.state.namedValues, this.state.defaultName)
+      )
+    }
+
+    return <Container>
+      <div><TextField
+        name='anonymous-value'
+        value={this.state.value}
+        onChange={onChange}
+        {...this.state.textFieldProps}
+      /></div>
+
+      <SelectField
+        name='named-value'
+        value={this.state.value}
+        onChange={onChange}
+        {...this.state.selectFieldProps}
+      >{
+        Array.from(this.state.namedValues)
+          .map(([value, name]) => (<MenuItem
+            value={value}
+            primaryText={name}
+            key={name}
+            {...this.state.createMenuItemProps({value, name})}
+          />))
+      }</SelectField>
+
+    </Container>
+  }
+
+  createState (value, namedValues, defaultName) {
+    const name = namedValues.has(value)
+      ? namedValues.get(value)
+      : defaultName
+
+    return {
+      value,
+      namedValues,
+      defaultName,
+      name
+    }
+  }
+}
