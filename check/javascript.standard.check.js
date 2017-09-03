5e3d6faedc932f8072e334921426f6bda7687167
khai96_ <hvksmr1996@gmail.com>
2017-09-03 22:33:45 +0700

    Make standard check customizable

--- check/javascript.standard.check.js
+++ check/javascript.standard.check.js
@@ -1,11 +1,27 @@
 'use strict'
+const path = require('path')
 const {spawn} = require('child_process')
+const {
+  env: {
+    STANDARDJS_EXECUTABLE,
+    STANDARDJS_ARGV,
+    SKIP_CODE_STYLE_CHECKING
+  }
+} = require('process')
 
-test('JavaScript Code Style: StandardJS', async () => {
+const wdir = path.resolve(__dirname, '..')
+
+SKIP_CODE_STYLE_CHECKING !== 'true' && test('JavaScript Code Style: StandardJS', async () => {
   let stdout = ''
   let stderr = ''
 
-  const child = spawn('standard')
+  const argv = STANDARDJS_ARGV
+    ? JSON.parse(STANDARDJS_ARGV)
+    : []
+
+  expect(argv).toBeInstanceOf(Array)
+
+  const child = spawn(STANDARDJS_EXECUTABLE || 'standard', argv, {cwd: wdir})
   child.stdout.on('data', data => { stdout += data })
   child.stderr.on('data', data => { stderr += data })
 
