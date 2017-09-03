430d41f188a8a11967269c985bc5da09e3735986
khai96_ <hvksmr1996@gmail.com>
2017-09-03 22:58:02 +0700

    Convert to multi-line

--- check/javascript.standard.check.js
+++ check/javascript.standard.check.js
@@ -20,7 +20,12 @@ test('JavaScript Code Style: StandardJS', () => {
 
   expect(argv).toBeInstanceOf(Array)
 
-  const {stdout, stderr, status, error} = spawnSync(STANDARDJS_EXECUTABLE || 'standard', argv, {cwd: wdir})
+  const {
+    stdout,
+    stderr,
+    error,
+    status
+  } = spawnSync(STANDARDJS_EXECUTABLE || 'standard', argv, {cwd: wdir})
 
   if (stdout === null) console.warn('standard.stdout is null')
   if (stderr === null) console.warn('standard.stderr is null')
