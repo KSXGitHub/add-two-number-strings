933302622b84618bdb0a97b6bf4150b83cfff5d3
khai96_ <hvksmr1996@gmail.com>
2017-09-03 22:39:33 +0700

    Spawn standard synchronously
    to avoid timing out

--- check/javascript.standard.check.js
+++ check/javascript.standard.check.js
@@ -1,6 +1,6 @@
 'use strict'
 const path = require('path')
-const {spawn} = require('child_process')
+const {spawnSync} = require('child_process')
 const {
   env: {
     STANDARDJS_EXECUTABLE,
@@ -11,25 +11,17 @@ const {
 
 const wdir = path.resolve(__dirname, '..')
 
-SKIP_CODE_STYLE_CHECKING !== 'true' && test('JavaScript Code Style: StandardJS', async () => {
-  let stdout = ''
-  let stderr = ''
-
+SKIP_CODE_STYLE_CHECKING !== 'true' && test('JavaScript Code Style: StandardJS', () => {
   const argv = STANDARDJS_ARGV
     ? JSON.parse(STANDARDJS_ARGV)
     : []
 
   expect(argv).toBeInstanceOf(Array)
 
-  const child = spawn(STANDARDJS_EXECUTABLE || 'standard', argv, {cwd: wdir})
-  child.stdout.on('data', data => { stdout += data })
-  child.stderr.on('data', data => { stderr += data })
-
-  const status = await new Promise(
-    resolve => child.on('exit', status => resolve(status))
-  )
-
+  const {stdout, stderr, status} = spawnSync(STANDARDJS_EXECUTABLE || 'standard', argv, {cwd: wdir})
   if (status) {
+    if (stdout === null) console.warn('standard.stdout is null')
+    if (stderr === null) console.warn('standard.stderr is null')
     throw new Error(stderr + '\n' + stdout)
   }
 })
