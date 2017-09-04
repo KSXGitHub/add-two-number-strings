6b4d03e54f8ab6ab3380dff29405da62dc67bf62
khai96_ <hvksmr1996@gmail.com>
2017-09-04 18:43:07 +0700

    Re-implement MuiWrapper using class

--- src/components/MuiWrapper.jsx
+++ src/components/MuiWrapper.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import {darkBaseTheme as defaultTheme} from 'material-ui/styles/baseThemes/darkBaseTheme'
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
-export default function MuiWrapper (props) {
-  return <MuiThemeProvider muiTheme={getMuiTheme(defaultTheme)}>
-    {props.children}
-  </MuiThemeProvider>
+export default class MuiWrapper extends React.Component {
+  constructor (props) {
+    super(props)
+
+    const {
+      darkTheme = false,
+      ...rest
+    } = props
+
+    this.state = {darkTheme, ...rest}
+  }
+
+  render () {
+    const {
+      children,
+      darkTheme
+    } = this.state
+
+    return <MuiThemeProvider
+      muiTheme={getMuiTheme(darkTheme ? darkBaseTheme : lightBaseTheme)}
+    >
+      {children}
+    </MuiThemeProvider>
+  }
 }
