1a0b63e85a220190e842245ac2ff6b7e3e000147
khai96_ <hvksmr1996@gmail.com>
2017-09-04 18:56:09 +0700

    Re-implement App using class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,30 @@ import RandomNumberForm from './RandomNumberForm.jsx'
 import ClockForm from './ClockForm.jsx'
 import Footer from './Footer.jsx'
 
-export default function App ({init}) {
-  return <MuiWrapper><div>
-    <header>
-      <AppBar title='Hello, World!!' showMenuIconButton={false} />
-    </header>
-    <main>
-      <RandomNumberForm init={init} />
-      <ClockForm />
-    </main>
-    <Footer />
-  </div></MuiWrapper>
+export default class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    const {
+      init,
+      darkTheme = false
+    } = props
+
+    this.state = {init, darkTheme}
+  }
+
+  render () {
+    const {init, darkTheme} = this.state
+
+    return <MuiWrapper darkTheme={darkTheme}><div>
+      <header>
+        <AppBar title='Hello, World!!' showMenuIconButton={false} />
+      </header>
+      <main>
+        <RandomNumberForm init={init} />
+        <ClockForm />
+      </main>
+      <Footer />
+    </div></MuiWrapper>
+  }
 }
