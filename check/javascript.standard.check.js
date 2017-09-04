288b47ad8c836e59eee5200429e36711e3074754
khai96_ <hvksmr1996@gmail.com>
2017-09-04 21:54:25 +0700

    Create test-spawn.js and use in in javascript.standard.check.js

--- check/javascript.standard.check.js
+++ check/javascript.standard.check.js
@@ -1,36 +1,7 @@
 'use strict'
-const path = require('path')
-const {spawnSync} = require('child_process')
-const {
-  env: {
-    STANDARDJS_EXECUTABLE = 'standard',
-    STANDARDJS_ARGV,
-    SKIP_CODE_STYLE_CHECKING
-  }
-} = require('process')
+const main = require('./lib/test-spawn')
 
-const wdir = path.resolve(__dirname, '..')
-
-test('JavaScript Code Style: StandardJS', () => {
-  if (SKIP_CODE_STYLE_CHECKING === 'true') return
-
-  const argv = STANDARDJS_ARGV
-    ? JSON.parse(STANDARDJS_ARGV)
-    : []
-
-  expect(argv).toBeInstanceOf(Array)
-
-  const {
-    stdout,
-    stderr,
-    signal,
-    error,
-    status
-  } = spawnSync(STANDARDJS_EXECUTABLE, argv, {cwd: wdir, shell: true})
-
-  if (stdout === null) console.warn('standard.stdout is null')
-  if (stderr === null) console.warn('standard.stderr is null')
-  if (signal) console.warn(`standard.signal is ${JSON.stringify(signal)}`)
-  if (error) throw error
-  if (status) throw new Error(stderr + '\n' + stdout)
+main({
+  defaultExecutable: 'standard',
+  envPrefix: 'STANDARDJS'
 })
---
+++ check/lib/test-spawn.js
@@ -0,0 +1,43 @@
+'use strict'
+
+function main ({
+  path = require('path'),
+  childProcess: {spawnSync} = require('child_process'),
+  process: {env} = require('process'),
+  defaultExecutable = 'echo',
+  envPrefix = ''
+} = {}) {
+  const {
+    [`${envPrefix}_EXECUTABLE`]: executable = defaultExecutable,
+    [`${envPrefix}_ARGV`]: spawnArguments,
+    [`${envPrefix}_SKIP`]: skipSpawnTesting
+  } = env
+
+  const wdir = path.resolve(__dirname, '..')
+
+  test('JavaScript Code Style: StandardJS', () => {
+    if (skipSpawnTesting === 'true') return
+
+    const argv = spawnArguments
+      ? JSON.parse(spawnArguments)
+      : []
+
+    expect(argv).toBeInstanceOf(Array)
+
+    const {
+      stdout,
+      stderr,
+      signal,
+      error,
+      status
+    } = spawnSync(executable, argv, {cwd: wdir, shell: true})
+
+    if (stdout === null) console.warn('respose.stdout is null')
+    if (stderr === null) console.warn('respose.stderr is null')
+    if (signal) console.warn(`respose.signal is ${JSON.stringify(signal)}`)
+    if (error) throw error
+    if (status) throw new Error(stderr + '\n' + stdout)
+  })
+}
+
+module.exports = main
