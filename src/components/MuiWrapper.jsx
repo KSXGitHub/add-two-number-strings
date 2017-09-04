d92452c62135e6223ed99bbfeda35edf064a7115
khai96_ <hvksmr1996@gmail.com>
2017-09-04 19:11:13 +0700

    Since there isn't a state, convert to function

--- src/components/MuiWrapper.jsx
+++ src/components/MuiWrapper.jsx
@@ -4,19 +4,17 @@ import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
-export default class MuiWrapper extends React.Component {
-  render () {
-    const {
-      darkTheme,
-      children,
-      ...rest
-    } = this.props
+export default function MuiWrapper (props) {
+  const {
+    darkTheme,
+    children,
+    ...rest
+  } = this.props
 
-    return <MuiThemeProvider
-      muiTheme={getMuiTheme(darkTheme ? darkBaseTheme : lightBaseTheme)}
-      {...rest}
-    >
-      {children}
-    </MuiThemeProvider>
-  }
+  return <MuiThemeProvider
+    muiTheme={getMuiTheme(darkTheme ? darkBaseTheme : lightBaseTheme)}
+    {...rest}
+  >
+    {children}
+  </MuiThemeProvider>
 }
