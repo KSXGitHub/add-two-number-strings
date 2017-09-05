a3b47507cf478a3bd75d9a9322eea58b62cd4cb0
khai96_ <hvksmr1996@gmail.com>
2017-09-05 22:21:48 +0700

    Improve Buttons' disabled props

--- src/components/HashMassGeneratorForm.jsx
+++ src/components/HashMassGeneratorForm.jsx
@@ -70,14 +70,14 @@ export default class HashMassGeneratorForm extends React.Component {
           <RaisedButton
             label='Generate'
             onClick={() => this.setState({actualData: editableData})}
-            disabled={actualData === editableData}
+            disabled={!editableData || actualData === editableData}
             primary
           />
 
           <FlatButton
             label='Clear'
             onClick={() => this.setState({actualData: '', editableData: ''})}
-            disabled={!editableData}
+            disabled={!actualData && !editableData}
             secondary
           />
         </div>
