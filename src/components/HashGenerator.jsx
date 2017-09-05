2e079079363dfee592a0489ec57ab6389d7a1c15
khai96_ <hvksmr1996@gmail.com>
2017-09-05 21:14:11 +0700

    Create HashGenerator components

---
+++ src/components/HashGenerator.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import Paper from 'material-ui/Paper'
+import crypto from 'crypto'
+
+export const DEFAULT_DISPLAY = array => Array.from(array)
+  .map(byte =>
+    [byte >> 4, byte & 0x0F]
+      .map(bits => bits.toString(16))
+      .join('')
+  )
+  .join('')
+
+export const LISTENED_PROP_NAMES = [
+  'display',
+  'data',
+  'encoding',
+  'algorithm'
+]
+
+export default class HashGenerator extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: null
+    }
+  }
+
+  render () {
+    const {
+      props: {
+        display = DEFAULT_DISPLAY
+      },
+      state: {
+        value
+      }
+    } = this
+
+    return <Paper>
+      <output>
+        {value
+          ? display(value)
+          : <i>(Empty)</i>
+        }
+      </output>
+    </Paper>
+  }
+
+  componentDidMount () {
+    this.updateState(this.props)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    const {props} = this
+
+    for (const name of LISTENED_PROP_NAMES) {
+      if (props[name] !== nextProps[name]) {
+        this.updateState(nextProps)
+        return
+      }
+    }
+  }
+
+  updateState (props) {
+    const {
+      data = '',
+      encoding = 'utf8',
+      algorithm = 'sha1'
+    } = props
+
+    if (!data) {
+      this.setState({value: null})
+      return Promise.resolve(null)
+    }
+
+    const hash = crypto.createHash(algorithm)
+
+    return new Promise(resolve =>
+      hash.end(data, encoding, () => {
+        const value = hash.read()
+        this.setState({value})
+        resolve(value)
+      })
+    )
+  }
+}
---
+++ src/components/HashMassGeneratorForm.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
+import FlatButton from 'material-ui/FlatButton'
+import Paper from 'material-ui/Paper'
+import TextField from 'material-ui/TextField'
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card'
+import HashMassGenerator from './HashMassGenerator.jsx'
+
+export default class HashMassGeneratorForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      editableData: '',
+      actualData: ''
+    }
+  }
+
+  render () {
+    const {
+      editableData,
+      actualData
+    } = this.state
+
+    return <Paper zDepth={1}><Card>
+      <CardHeader
+        title='Hash Generator'
+        subtitle='Expand this card to generate cryptographic hashes'
+        actAsExpander
+        showExpandableButton
+      />
+
+      <CardActions expandable>
+        <div>
+          <label htmlFor='hash-data'>Data:</label>
+        </div>
+
+        <div>
+          <TextField
+            hintText='Data to be hash...'
+            value={editableData}
+            onChange={(_, editableData) => this.setState({editableData})}
+          />
+        </div>
+
+        <div>
+          <RaisedButton
+            label='Generate'
+            onClick={() => this.setState({actualData: editableData})}
+            primary
+          />
+
+          <FlatButton
+            label='Clear'
+            onClick={() => this.setState({actualData: ''})}
+            secondary
+          />
+        </div>
+      </CardActions>
+
+      <CardText expandable>
+        <HashMassGenerator
+          data={actualData}
+        />
+      </CardText>
+    </Card></Paper>
+  }
+}
