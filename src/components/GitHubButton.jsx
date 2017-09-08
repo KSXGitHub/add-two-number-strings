e162ed1a9d3061aa267d71e00ef8df59c4348b6b
khai96_ <hvksmr1996@gmail.com>
2017-09-08 09:48:09 +0700

    Set default color

--- src/components/GitHubButton.jsx
+++ src/components/GitHubButton.jsx
@@ -2,14 +2,27 @@ import React from 'react'
 import IconButton from 'material-ui/IconButton'
 import Icon from 'mui-icons/cmdi/github'
 
+export const DEFAULT_ICON_STYLE = {
+  fill: 'white',
+  color: 'white'
+}
+
 export default function GitHubButton (props) {
   const {
     buttonProps = {},
     iconProps = {},
+    iconStyle = {},
     ...rest
   } = props
 
-  return <IconButton {...buttonProps} {...rest}>
+  return <IconButton
+    iconStyle={{
+      ...DEFAULT_ICON_STYLE,
+      ...iconStyle
+    }}
+    {...buttonProps}
+    {...rest}
+  >
     <Icon {...iconProps} />
   </IconButton>
 }
