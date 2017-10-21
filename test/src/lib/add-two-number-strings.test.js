03387ec441c6c5f44499b60e8661c998e2b3bb47
khai96_ <hvksmr1996@gmail.com>
2017-10-21 16:50:44 +0700

    Complete correct use-cases

--- test/src/lib/add-two-number-strings.test.js
+++ test/src/lib/add-two-number-strings.test.js
@@ -1,13 +1,50 @@
 'use strict'
+const {RangeIterable, ProductIterable} = require('x-iterable')
 const add = require('../../../src/lib/add-two-number-strings')
 
-test('Correct use cases', () => {
-  const unit = (a, b) =>
-    expect(add(String(a), String(b))).toStrictEqual(String(a + b))
+describe('Correct use-cases', () => {
+  const unit = (a, b) => {
+    const sum = a + b
+    return test(
+      `${a} + ${b} === ${sum}`,
+      () => expect(add(String(a), String(b))).toBe(String(sum))
+    )
+  }
 
   const multipleUnits = array => array.forEach(x => unit(...x))
 
-  test('Same length; Sum of each digit-pair is less than 10', () => multipleUnits([
-    [123, 456], [111, 222], [0, 1], [333, 111], [2, 5]
-  ]))
+  describe(
+    'One-digit numbers',
+    () => multipleUnits(
+      ProductIterable.pow(RangeIterable.range(10), 2)
+    )
+  )
+
+  describe(
+    'Same length; Sum of each digit-pair is less than 10',
+    () => multipleUnits([
+      [123, 456], [111, 222], [0, 1], [333, 111], [2, 5]
+    ])
+  )
+
+  describe(
+    'Same length; Sum of some digit-pairs are greater than or equal to 10',
+    () => multipleUnits([
+      [99, 11], [288, 112], [789, 111], [2999, 3125], [1122, 9988]
+    ])
+  )
+
+  describe(
+    'Different length; Sum of each digit-pair is less than 10',
+    () => multipleUnits([
+      [123, 2], [456, 122333], [0, 22553], [15, 654321], [1, 5]
+    ])
+  )
+
+  describe(
+    'Different length; Sum of each digit-pair is greater than 10',
+    () => multipleUnits([
+      [123, 7], [1999, 2], [6789, 222], [5555, 345], [12, 88888]
+    ])
+  )
 })
